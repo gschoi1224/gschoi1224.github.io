59b24745febe505db8587affa57a8dc2bb971645
CGS <gschoi1224@gmail.com>
2021-10-20 14:17:07 +0900

    힙 정렬

--- docs/Algorithm/Sorting/heap.md
+++ docs/Algorithm/Sorting/heap.md
@@ -1,6 +1,6 @@
 ---
 layout: default
-nav_order: 2
+nav_order: 4
 comments: false 
 title: '힙정렬'
 has_children: true
@@ -11,10 +11,16 @@ grand_parent: '알고리즘'
 # 힙 정렬
 {: .fw-700 .fs-7 }
 
-> 최대 힙 트리나 최소 힙 트리를 구성해 정렬을 하는 방법
+![heap](https://upload.wikimedia.org/wikipedia/commons/4/4d/Heapsort-example.gif)
+> [최대 힙](../../Structure/heap.md) 트리나 최소 힙 트리를 구성해 정렬을 하는 방법, 이진 트리를 최대 힙으로 만들기 위해 재구성하는 과정이 트리의 깊이만큼 이루어지므로 O(log n)의 수행시간이 걸리며 일반적인 경우 힙 정렬은 O(n log n)의 시간복잡도를 가진다.
+
+## 알고리즘
 
 1. 완전 이진 트리를 구성한다.
-2. 최대 힙을 
+2. 내림차순 정렬을 위해서는 최대 힙, 오름차순 정렬을 위해서는 최소 힙을 구성한다.
+3. 가장 큰 수(루트)를 가장 작은 수와 교환한다.
+4. 2, 3을 반복한다.
+
 
 ### 출처
 - [wikipedia](https://ko.wikipedia.org/wiki/힙_정렬)
--- docs/Algorithm/Sorting/sort.js
+++ docs/Algorithm/Sorting/sort.js
@@ -1,15 +1,76 @@
-function selectionSort(arr) {
-    for (let i = 0 ; i < arr.length; i++) {
-        let minIndex = i;
-        for (let j = i + 1 ; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
-                minIndex = j;
-            }
-        }
-        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-        console.log(i, minIndex, arr);
+class MaxHeap {
+    constructor () {
+        this.array = [];
     }
-    return arr;
-}
+    getLeftChildIndex(index) {
+        return index * 2;
+    }
+    getRightChildIndex(index) {
+        return index * 2 + 1;
+    }
+    getParentIndex(index) {
+        return Math.floor(index / 2);
+    }
+    insertNode(value) {
+        // 부모 노드와 삽입 노드를 재귀적으로 비교해 부모 노드가 삽입 노드보다 작을 때까지 위치를 바꿔준다
+        if (this.array.length === 0) {
+            this.array[1] = value;
+        } else {
+            this.array.push(value);
+            this.shiftUp();
+        }
+    }
+    shiftUp(index = this.array.length - 1) {
+        const parentNodeIndex = this.getParentIndex(index);
+        const myValue = this.array[index];
+        const parentValue = this.array[parentNodeIndex];
+        if (parentNodeIndex !== index && myValue > parentValue) {
+            [this.array[parentNodeIndex], this.array[index]] = [myValue, parentValue];
+            this.shiftUp(parentNodeIndex);
+        }
+    }
+    popNode() {
+        const myValue = this.array[1];
+        const lastIndex = this.array.length - 1;
+        if (lastIndex === 1) {
+            this.array = [];
+        }
+        else if (lastIndex === 0) {
+            return null;
+        }
+        else {
+            this.array[1] = this.array.pop();
+            this.shiftDown();
+        }
+        return myValue;
+    }
+    shiftDown(index = 1) {
+        const leftChildIndex = this.getLeftChildIndex(index);
+        const rightChildIndex = this.getRightChildIndex(index);
+        const myValue = this.array[index];
+        let targetIndex;
+        let targetValue;
+        if (this.array[leftChildIndex] > this.array[rightChildIndex]) {
+            targetValue = this.array[leftChildIndex]
+            targetIndex = leftChildIndex;
+        } else {
+            targetValue = this.array[rightChildIndex];
+            targetIndex = rightChildIndex;
+        }
 
-selectionSort([5, 3, 2, 4, 1]);
+        if (targetValue > myValue) {
+            [this.array[index], this.array[targetIndex]] = [targetValue, myValue];
+            this.shiftDown(targetIndex);
+        }
+    }
+}
+const heap = new MaxHeap();
+heap.insertNode(6)
+heap.insertNode(5)
+heap.insertNode(3)
+heap.insertNode(1)
+heap.insertNode(8)
+heap.insertNode(7)
+heap.insertNode(2)
+heap.insertNode(4)
+console.log(heap);  // 8, 6, 7, 4, 5, 3, 2, 1
